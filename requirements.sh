97da7d09180eb0b6a4e629b5f69f46b9381ab00f
wighawag <wighawag@gmail.com>
2023-04-25 10:20:00 +0100

    remove the use of just

--- requirements.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-curl -L https://foundry.paradigm.xyz | bash;
-PATH="$PATH:$HOME/.foundry/bin" foundryup;
-
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y;
-PATH="$PATH:$HOME/.cargo/bin" cargo install --locked just;
-PATH="$PATH:$HOME/.cargo/bin" cargo install --locked ldenv;
-
