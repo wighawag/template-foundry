3cda513f3914c171ab2e819acd9d695e178ec76b
wighawag <wighawag@gmail.com>
2023-04-28 11:43:41 +0100

    fix wezterm

--- wezterm.lua
+++ wezterm.lua
@@ -1,32 +1,39 @@
 local wezterm = require 'wezterm'
 local mux = wezterm.mux
 
-wezterm.on('gui-startup', function(cmd)
+wezterm.on('gui-startup', function()
+  
+  local shell = os.getenv("SHELL") or "bash";
+  local cwd = os.getenv("PWD");
+  local current_dir=io.popen"cd":read'*l'
+  if current_dir then
+    cwd = string.gsub(string.gsub(current_dir, "C:\\", "/c/"), "\\", "/")
+  end
+
+  
   local tab, pane, window
   tab, pane, window = mux.spawn_window {
-    args = {'bash', '-i', '-c' , 'cd '.. cmd.args[1] .. '; bash'},
-   
-    
+    cwd = current_dir,
+    args = {'bash', '-i', '-c', 'cd ' .. cwd ..';' .. shell}
   }
   tab:set_title 'template-factory'
   window:set_title 'template-factory'
-
+  
   local watch_deploy_pane = pane:split {
-    args = {'bash', '-i', '-c', 'cd '.. cmd.args[1] .. '; sleep 1; pnpm watch_deploy; bash'},
+    args = {'bash', '-i', '-c', 'cd ' .. cwd ..'; sleep 1; pnpm watch_deploy; ' ..shell},
     direction = 'Bottom'
   }
 
   local anvil_pane = watch_deploy_pane:split {
-    args= {'bash', '-i', '-c', 'anvil; bash'},
+    args = {'bash', '-i', '-c', 'cd ' .. cwd ..'; anvil; ' ..shell},
     direction = 'Right'
   }
 
   local watch_compile_pane = watch_deploy_pane:split {
-    args = {'bash', '-i', '-c', 'cd '.. cmd.args[1] .. '; sleep 1; pnpm watch_compile; bash'},
+    --args = {'bash', '-i', '-c', 'sleep 1; pnpm watch_compile; bash'},
+    args = {'bash', '-i', '-c', 'cd ' .. cwd ..'; sleep 1; pnpm watch_compile; ' ..shell},
     direction = 'Bottom'
   }
-  
-  
 
   
 end)
